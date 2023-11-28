60019ca8916aa57cdb56e24fb98512017c37c61d
cl4pper <cl4pper@gmail.com>
2023-11-28 19:08:00 +0000

    feat: command to generate a react component
    * create .tsx .sass .test.tsx files
    * error message if component name was not set

---
+++ src/commands/component:ts.ts
@@ -0,0 +1,23 @@
+import { GluegunToolbox } from 'gluegun'
+
+module.exports = {
+  name: 'component:ts',
+  description: 'Creates .tsx, .sass, .test.tsx into a folder.',
+  run: async (toolbox: GluegunToolbox) => {
+    const {
+      print: { info },
+      parameters,
+      generateComponent
+    } = toolbox
+
+    const name = parameters.first
+
+    if (!name) {
+      return info('A name for component is required!')
+    }
+
+    generateComponent();
+
+    info(`Component ${name} folder has been created.`)
+  },
+}
